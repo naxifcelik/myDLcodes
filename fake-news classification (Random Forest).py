5d36c1d4e36d156a8eab095be15b130c9ef4951b
Nazif ÇELİK <48800008+naxifcelik@users.noreply.github.com>
2023-05-17 20:08:18 +0300

    fake-news classification (Random Forest).py

--- fake-news classification (Random Forest).py
+++ fake-news classification (Random Forest).py
@@ -1,59 +1,59 @@
-#Nazif ÇELİK
-#library to perform necessary operations on the dataset
 import pandas as pd
-#I will do multithreading in order to use the processor more efficiently 
-#and get fast results. I include the necessary library for this.
-from multiprocessing import Process
-
-# Loading the dataset
-df = pd.read_csv('WELFake_Dataset.csv')
-
-# Removing missing data from the dataset
-df.dropna(inplace=True)
-
-# Checking the number of samples in the dataset
-print(f"number of samples in the dataset: {len(df)}")
-
-# Show the first five samples of the dataset
-print(df.head())
-
+from multiprocessing import Process, Queue
 from sklearn.model_selection import train_test_split
-#I divided the dataset into train and test parts
-title=df['title']
-typr=df['label']
-X_train, X_test, y_train, y_test = train_test_split(title, typr, test_size=0.25, random_state=42,shuffle=True)
-
-
 from sklearn.feature_extraction.text import CountVectorizer
-
-# Fitting CountVectorizer on our train data
-vectorizer = CountVectorizer()
-X_train_vect = vectorizer.fit_transform(X_train)
-
-# Vectorizing test data
-X_test_vect = vectorizer.transform(X_test)
-
-
 from sklearn.ensemble import RandomForestClassifier
-
-# Creating the Random Forest classifier
-rfc = RandomForestClassifier(n_estimators=100)
-
-# Training the Random Forest classifier on the train set
-rfc.fit(X_train_vect, y_train)
-
-
 from sklearn.metrics import accuracy_score, classification_report
 
-# Predict on the test set
-y_pred = rfc.predict(X_test_vect)
+# Function to vectorize and train the model
+def vectorize_and_train(X_train, X_test, y_train, queue):
+    # Vectorize the training data
+    vectorizer = CountVectorizer()
+    X_train_vect = vectorizer.fit_transform(X_train)
+    
+    # Vectorize the test data
+    X_test_vect = vectorizer.transform(X_test)
+    
+    # Train the Random Forest classifier
+    rfc = RandomForestClassifier(n_estimators=100)
+    rfc.fit(X_train_vect, y_train)
+    
+    # Make predictions on the test data
+    y_pred = rfc.predict(X_test_vect)
+    
+    # Put the predictions in the queue for retrieval
+    queue.put(y_pred)
 
-# Calculating the accuracy of the model
+# Load the dataset
+df = pd.read_csv('WELFake_Dataset.csv')
+df.dropna(inplace=True)
+print(f"Number of samples in the dataset: {len(df)}")
+print(df.head())
+
+# Split the data into train and test sets
+title = df['title']
+typr = df['label']
+X_train, X_test, y_train, y_test = train_test_split(title, typr, test_size=0.25, random_state=42, shuffle=True)
+
+# Create a queue to store the predictions
+queue = Queue()
+
+# Create a process for vectorization and training
+p = Process(target=vectorize_and_train, args=(X_train, X_test, y_train, queue))
+
+# Start the process
+p.start()
+
+# Wait for the process to finish and retrieve the predictions
+y_pred = queue.get()
+
+# Join the process
+p.join()
+
+# Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
-print(f"accuracy of the model: {accuracy}")
+print(f"Model accuracy: {accuracy}")
 
-# Generating a classification report
+# Generate classification report
 report = classification_report(y_test, y_pred)
 print(report)
-
-
